3be117a00bca712059203c2442b4ab4d5da58d82
Craig Collie <craig.collie@rea-group.com>
2017-05-03 11:42:52 +1000

    Remove unnecessary files

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import matchRoute from './../utils/matchRoute';
+import getParamsFromUrl from './../utils/getParamsFromUrl';
 
 class Router extends Component {
   render() {
@@ -19,14 +20,14 @@ class Router extends Component {
 
     //  Routes can be pattern matched
     //  So a /path/:id can be used
-    const {
-      component: ComponentView
-    } = matchRoute(getRoutes(), location.pathname);
+    const { path, component: ComponentView } = matchRoute(getRoutes(), location.pathname);
+    const routeParams = getParamsFromUrl(path, location.pathname);
 
     return (
       <ComponentView
         location={location}
         resolvedData={resolvedData}
+        routeParams={routeParams}
       />
     );
   }
