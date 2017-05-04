e47899888fbead87d4b29014b67021a25e60f04d
Craig Collie <craig.collie@rea-group.com>
2017-05-04 11:08:22 +1000

    Fix unit tests

--- src/utils/matchRoute-spec.js
+++ src/utils/matchRoute-spec.js
@@ -4,22 +4,24 @@ import { expect } from 'chai';
 import matchRoute from './matchRoute';
 import Route from './../components/Route';
 
+const Test = () => (<div></div>);
+
 describe('matchRoute', () => {
   describe('single route', () => {
     const Routes = () => (
-      <Route path="/foo/:id" />
+      <Route path="/foo/:id" component={Test} />
     );
 
     const tests = [
       {
         path: '/foo/1',
-        result: { path: '/foo/:id' }
+        result: { path: '/foo/:id', component: Test }
       },
     ];
 
     tests.forEach(({ path, result }) => {
       it(`should match ${path} to ${result.path}`, () => {
-        expect(matchRoute(Routes().props.children, path)).to.eql(result);
+        expect(matchRoute(Routes, path)).to.eql(result);
       });
     });
   });
@@ -27,20 +29,20 @@ describe('matchRoute', () => {
   describe('multiple routes', () => {
     const Routes = () => (
       <div>
-        <Route path="/" />
-        <Route path="/foo/:id" />
-        <Route path="/foo/:bar/:id" />
+        <Route path="/" component={Test} />
+        <Route path="/foo/:id" component={Test} />
+        <Route path="/foo/:bar/:id" component={Test} />
       </div>
     );
 
     const tests = [
       {
         path: '/',
-        result: { path: '/' },
+        result: { path: '/', component: Test },
       },
       {
         path: '/foo/1',
-        result: { path: '/foo/:id' },
+        result: { path: '/foo/:id', component: Test },
       },
       {
         path: '/no-route',
@@ -48,13 +50,13 @@ describe('matchRoute', () => {
       },
       {
         path: '/foo/somewhere/else',
-        result: { path: '/foo/:bar/:id' },
+        result: { path: '/foo/:bar/:id', component: Test },
       },
     ];
 
     tests.forEach(({ path, result }) => {
       it(`should match ${path} to ${result.path}`, () => {
-        expect(matchRoute(Routes().props.children, path)).to.eql(result);
+        expect(matchRoute(Routes, path)).to.eql(result);
       });
     });
   });
