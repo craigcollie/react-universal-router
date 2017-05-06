3c42f8ef1fa21cfe84c13f5d7aae44797f1afd66
Craig Collie <craig.collie@rea-group.com>
2017-05-07 09:57:49 +1000

    Add prod config

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,21 @@
 import path from 'path';
+import nodeExternals from 'webpack-node-externals';
+
+const env = process.env.NODE_ENV;
 
 export default {
   entry: './src/index.js',
   output: {
-    filename: 'tiny-universal.js',
+    filename: `tiny-universal${(env === 'production') ? 'min' : ''}.js`,
     path: path.resolve(__dirname, 'dist'),
     libraryTarget: 'umd',
     library: 'TinyUniversal',
   },
+  target: 'node',
+  externals: [
+    nodeExternals(),
+  ],
+  devtool: 'cheap-eval-source-map',
   module: {
     rules: [
       {
