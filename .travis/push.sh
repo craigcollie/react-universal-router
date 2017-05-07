bf6e0afc0cfc428eae75791c495a39e9007ee7d4
Craig Collie <craig.collie@rea-group.com>
2017-05-07 12:07:18 +1000

    Change script

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,21 +1,14 @@
 #!/bin/sh
 
-setup_git() {
-  git config --global user.email "hysterix3@gmail.com"
-  git config --global user.name "CraigCollie"
-}
+# Set git user
+git config --global user.email "hysterix3@gmail.com"
+git config --global user.name "CraigCollie"
 
-commit_website_files() {
-  git checkout -b deploy
-  git add .
-  git commit --message "Adding build artifacts: $TRAVIS_BUILD_NUMBER"
-}
+# Check out deploy branch
+git checkout -b deploy
+git add .
+git commit --message "Adding build artifacts: $TRAVIS_BUILD_NUMBER"
 
-upload_files() {
-  git remote add origin https://1457abec2e59d60904fdc06857c73dadbb11a971@github.com/craigcollie/tiny-universal.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin deploy
-}
-
-setup_git
-commit_website_files
-upload_files
+# Commit branch
+git remote add origin https://1457abec2e59d60904fdc06857c73dadbb11a971@github.com/craigcollie/tiny-universal.git > /dev/null 2>&1
+git push --quiet --set-upstream origin deploy
